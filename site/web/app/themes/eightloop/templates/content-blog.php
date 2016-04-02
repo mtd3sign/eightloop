0be27db2c3a213c53b940824a6d73a99d9411e11
Mathew Thomas <hello@mathewthomasdesign.com>
2016-04-02 20:18:49 +0100

    added eightloop theme from previous development area

---
+++ site/web/app/themes/eightloop/templates/content-blog.php
@@ -0,0 +1,44 @@
+<div class="container-blog-home">
+
+  <div class="blog-home">
+
+  <?php
+
+  $args = array(
+    'post_type' => 'blog',
+    'posts_per_page' => '6',
+  );
+
+  $query = new WP_Query($args);
+
+  ?>
+
+    <?php
+        // The Loop
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                echo '<div class="blog-item">';
+                echo '<h2><a href="'.get_the_permalink().'">'.get_the_title().'</a></h2>';
+                echo '<p>'.get_the_excerpt().'</p>';
+                echo '<p class="blog-date">'.get_the_date('F Y').'</p>';
+
+                // $image = get_field('image');
+                $image = 'https://unsplash.it/200/300/?random';
+                $size = 'full'; // (thumbnail, medium, large, full or custom size)
+
+            if ($image) {
+                echo wp_get_attachment_image($image, $size);
+            }
+                echo '</div>';
+            }
+        } else {
+            // no posts found
+        }
+        /* Restore original Post Data */
+        wp_reset_postdata();
+
+        ?>
+
+</div>
+</div>
---
+++ site/web/app/themes/eightloop/templates/footer.php
@@ -0,0 +1,13 @@
+<footer class="container-footer">
+  <div class="footer">
+    <ul class="footer-items">
+      <li>Mathew Thomas</li>
+      <li>|</li>
+      <a href="mailto:hello@eightloop.io"><li>hello@eightloop.io</li>
+      <li>|</li>
+      <a href="https://twitter.com/_eightloop" target="_blank"><li>@_eightloop</li></a>
+    </ul>
+    <hr>
+    <p class="copyright">&copy; Mathew Thomas 2016</p>
+  </div>
+</footer>
