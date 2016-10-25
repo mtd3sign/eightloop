e3bf20e12a20a2f2350032ac4991c83ac7f7df5e
Mathew Thomas <hello@mathewthomasdesign.com>
2016-10-25 09:27:23 +0100

    roll back Wordpress to 4.4.2

--- site/web/wp/wp-admin/revision.php
+++ site/web/wp/wp-admin/revision.php
@@ -40,8 +40,8 @@ case 'restore' :
 	if ( ! $post = get_post( $revision->post_parent ) )
 		break;
 
-	// Restore if revisions are enabled or this is an autosave.
-	if ( ! wp_revisions_enabled( $post ) && ! wp_is_post_autosave( $revision ) ) {
+	// Revisions disabled (previously checked autosaves && ! wp_is_post_autosave( $revision ))
+	if ( ! wp_revisions_enabled( $post ) ) {
 		$redirect = 'edit.php?post_type=' . $post->post_type;
 		break;
 	}
@@ -63,7 +63,7 @@ default :
 	if ( ! $post = get_post( $revision->post_parent ) )
 		break;
 
-	if ( ! current_user_can( 'read_post', $revision->ID ) || ! current_user_can( 'edit_post', $revision->post_parent ) )
+	if ( ! current_user_can( 'read_post', $revision->ID ) || ! current_user_can( 'read_post', $post->ID ) )
 		break;
 
 	// Revisions disabled and we're not looking at an autosave
